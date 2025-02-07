5438e16b3457f7b4cbebf1a6b177f890df38f4f3
Drinix <thedrinix@gmail.com>
2025-02-07 20:37:13 +0100

    [Dashboard] Add vueuse package

--- dashboard/nuxt.config.ts
+++ dashboard/nuxt.config.ts
@@ -2,10 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils', '@prisma/nuxt', '@nuxt/ui'],
+  modules: ['nuxt-auth-utils', '@prisma/nuxt', '@nuxt/ui', '@vueuse/nuxt'],
   runtimeConfig: {
     botToken: '',
-    apiKey: ''
+    apiKey: '',
+    oauthDiscordClientId: '',
+    oauthDiscordClientSecret: '',
   },
   css: ['~/assets/css/main.css'],
 })
