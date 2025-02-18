26a2e12200ee49821fe6ff42e6566b19911e1dd4
Drinix <thedrinix@gmail.com>
2025-02-18 15:57:58 +0100

    [Dashboard] Create api endpoint for retrieving guild channels

---
+++ dashboard/server/api/guilds/[id]/channels.get.ts
@@ -0,0 +1,48 @@
+import { DiscordChannel } from '~/types/discord';
+import { FetchError } from 'ofetch';
+
+export default defineEventHandler(async event => {
+  const session = await requireUserSession(event);
+
+  const guildId = getRouterParam(event, 'id');
+  if (!guildId) {
+    throw createError({
+      message: 'Guild ID is required',
+      status: 400
+    });
+  }
+
+  const config = useRuntimeConfig();
+  const { botToken } = config;
+
+  if (!botToken) {
+    throw createError({
+      message: 'Bot token is not defined',
+      status: 500
+    });
+  }
+
+  try {
+    const channels = await $fetch<DiscordChannel[]>(`https://discord.com/api/guilds/${guildId}/channels`, {
+      headers: {
+        Authorization: `Bot ${botToken}`
+      }
+    });
+
+    return channels;
+  } catch (e) {
+    console.error(e);
+
+    if (e instanceof FetchError && e.status === 404) {
+      throw createError({
+        message: 'Guild not found',
+        status: 404
+      });
+    }
+
+    throw createError({
+      message: 'Failed to load channels',
+      status: 500
+    });
+  }
+})
