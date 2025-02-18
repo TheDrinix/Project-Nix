bf5ccf281d8f748b23304b03760c99f85c37429a
Drinix <thedrinix@gmail.com>
2025-02-18 15:53:21 +0100

    [Dashboard] Add pinia

--- dashboard/nuxt.config.ts
+++ dashboard/nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['nuxt-auth-utils', '@prisma/nuxt', '@nuxt/ui', '@vueuse/nuxt'],
+  modules: [
+    'nuxt-auth-utils',
+    '@prisma/nuxt',
+    '@nuxt/ui',
+    '@vueuse/nuxt',
+    '@pinia/nuxt'
+  ],
   runtimeConfig: {
     botToken: '',
     apiKey: '',
