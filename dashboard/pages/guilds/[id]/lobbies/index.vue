82455735dd14b2fd4827581b835e8ced8cfeebd6
Drinix <thedrinix@gmail.com>
2025-04-23 11:37:32 +0200

    [Dashboard] Add an option to refresh loaded lobbies

--- dashboard/pages/guilds/[id]/lobbies/index.vue
+++ dashboard/pages/guilds/[id]/lobbies/index.vue
@@ -6,26 +6,38 @@ const lobbyStore = useLobbyStore();
 
 const guildId = computed(() => route.params.id as string);
 
-const { data, status, error } = useLazyAsyncData(`lobbies`, () => {
-  return lobbyStore.fetchLobbies(guildId.value);
+let forceRefresh = false;
+
+const { data, status, error, refresh } = useLazyAsyncData(`lobbies`, () => {
+  return lobbyStore.fetchLobbies(guildId.value, forceRefresh);
 }, {
   watch: [guildId],
 });
 
-const lobbies = computed(() => {
-  return lobbyStore.getGuildLobbies(guildId.value);
-});
+const refreshLobbies = async () => {
+  forceRefresh = true;
+
+  await refresh();
+
+  forceRefresh = false
+}
 </script>
 
 <template>
   <div class="w-full">
-    <div class="flex justify-between items-center">
+    <div class="flex items-center">
       <h3 class="text-lg font-medium">Lobbies</h3>
-      <UButton class="my-2" :to="{ name: 'guilds-id-lobbies-create', params: { id: guildId } }" size="sm">Create lobby</UButton>
+      <UTooltip text="Refresh lobby list" :popper="{ placement: 'right', arrow: true }">
+        <UButton @click="refreshLobbies" icon="i-heroicons-arrow-path" variant="link" square />
+      </UTooltip>
+      <UButton class="ml-auto" :to="{ name: 'guilds-id-lobbies-create', params: { id: guildId } }" size="sm">Create lobby</UButton>
     </div>
-    <UDivider />
+    <UDivider class="my-2" />
     <div v-if="status === 'success'" class="flex flex-col gap-2">
-      <Lobby v-for="lobby in lobbies" v-bind="lobby" :key="lobby.id" />
+      <Lobby v-for="lobby in data" v-bind="lobby" :key="lobby.id" />
+      <div v-if="!data?.length" class="text-center text-gray-500">
+        There are currently no lobbies in this guild!
+      </div>
     </div>
     <div v-else-if="status === 'pending'" class="flex flex-col gap-2">
       <USkeleton v-for="i in 3" :key="i" class="h-[13.5rem] w-full" />
