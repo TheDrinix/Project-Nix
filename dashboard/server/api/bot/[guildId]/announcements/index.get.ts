706d5f65cf0d6ce6cd38fcd9db25fbe41429056e
Drinix <thedrinix@gmail.com>
2025-05-13 08:31:03 +0200

    [Dashboard] Create bot api endpoint for getting announcement config

--- dashboard/server/api/bot/[guildId]/announcements/index.get.ts
+++ dashboard/server/api/bot/[guildId]/announcements/index.get.ts
@@ -3,6 +3,10 @@ import prisma from "~/lib/prisma";
 export default defineEventHandler(async (event) => {
   const guildId = getRouterParam(event, 'guildId');
 
+  const query = getQuery(event);
+
+  const selection = query.selection;
+
   if (!guildId) {
     return createError({
       statusCode: 400,
@@ -10,10 +14,39 @@ export default defineEventHandler(async (event) => {
     });
   }
 
-  const announcementsConfig = await prisma.annoucementsConfig.findFirst({
+
+  let select: any;
+  switch (selection) {
+    case 'join':
+      select = {
+        announceJoin: true,
+        joinMessageEmbed: true,
+        channelId: true
+      }
+      break;
+    case 'leave':
+      select = {
+        announceLeave: true,
+        leaveMessageEmbed: true,
+        channelId: true
+      }
+      break;
+    case 'ban':
+      select = {
+        announceBan: true,
+        banMessageEmbed: true,
+        channelId: true
+      }
+      break
+    default:
+      select = undefined;
+  }
+
+  const announcementsConfig = await prisma.announcementsConfig.findFirst({
     where: {
       guildId: guildId,
-    }
+    },
+    select
   });
 
   if (!announcementsConfig) {
