fe1a8c6bf1e964726d97c28f47058569fe871191
Drinix <thedrinix@gmail.com>
2025-06-06 12:43:37 +0200

    [Dashboard] Fix embed validation schema

--- dashboard/utils/embed.ts
+++ dashboard/utils/embed.ts
@@ -1,12 +1,12 @@
 import { z } from 'zod';
 
 export const embedFieldSchema = z.object({
-  id: z.string().uuid(),
+  id: z.string().uuid().or(z.string().optional()),
   name: z.string().max(256).optional(),
   value: z.string().max(1024).optional(),
   inline: z.boolean().optional()
 }).refine(data => {
-  return !data.name && !!data.value;
+  return !!data.name || !!data.value;
 }, {
   message: 'Field name or content must be at least 1 character',
   path: ['name', 'value']
@@ -14,13 +14,13 @@ export const embedFieldSchema = z.object({
 
 export const embedSchema = z.object({
   title: z.string().max(100, 'Title must be at most 50 characters').optional(),
-  url: z.string().url('Invalid title URL').optional(),
+  url: z.string().url('Invalid title URL').or(z.string().optional()),
   description: z.string().max(1024, 'Description must be at most 1024 characters').optional(),
   color: z.number().int().min(0, 'Invalid hex color').max(0xFFFFFF, 'Invalid hex color'),
   timestamp: z.string().optional(),
   author: z.object({
     name: z.string().max(256, 'Author name must be at most 256 characters').optional(),
-    icon_url: z.string().url('Invalid author icon url').optional()
+    icon_url: z.string().url('Invalid author icon url').or(z.string().optional())
   })
     .refine(data => (!(!!data.icon_url && !data.name)), {
       message: 'Author name must be at least 1 character',
@@ -28,11 +28,11 @@ export const embedSchema = z.object({
     }),
   footer: z.object({
     text: z.string().max(200, 'Footer text must be at most 200 characters').optional(),
-    icon_url: z.string().url('Invalid footer icon url').optional()
+    icon_url: z.string().url('Invalid footer icon url').or(z.string().optional())
   }),
   fields: z.array(embedFieldSchema).default([]),
   image: z.object({
-    url: z.string().url('Invalid image URL').optional()
+    url: z.string().url('Invalid image URL').or(z.string().optional())
   }),
   thumbnail: z.object({
     url: z.string().optional().or(z.string().url('Invalid thumbnail URL'))
